da714f35762643451d5b7b2d9bb45428adffba90
tailuge <tailuge@gmail.com>
2025-01-04 14:33:15 +0000

    test

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
+declare var process: { env: { NODE_ENV: string } };
+declare var window: any;
+if (typeof process === 'undefined') {
+  window.process = { env: { NODE_ENV: 'production' } };
+}
 import Scorekeeper from './components/Scorekeeper';
-    export default Scorekeeper;
+export default Scorekeeper;
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 3000
   },
-  base: '/examples',
+  base: '/',
   build: {
     lib: {
       entry: './src/index.ts',
@@ -25,4 +25,5 @@ export default defineConfig({
     },
     outDir: 'dist',
   },
+  define: { 'process.env.NODE_ENV': '"production"' },
 });
